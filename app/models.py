c3e5f1fc41b3309ce70a755c110938a877544397
Turner <fanshaojie2014@gmail.com>
2016-07-12 21:32:30 +0800

    add user profile modules

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+import hashlib
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import request, current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
@@ -52,6 +54,12 @@ class User(UserMixin, db.Model):
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default=False)
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	name = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	about_me = db.Column(db.Text())
+	member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+	last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+	avatar_hash = db.Column(db.String(32))
 	
 	def __init__(self, **kwargs):
 		super(User, self).__init__(**kwargs)
@@ -60,7 +68,9 @@ class User(UserMixin, db.Model):
 				self.role = Role.query.filter_by(permissions=0xff).first()
 			if self.role is None:
 				self.role = Role.query.filter_by(default=True).first()
-
+		if self.email is not None and self.avatar_hash is None:
+			self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+	
 	@property
 	def password(self):
 		raise AttributeError('password is not a readable attribute')
@@ -122,6 +132,8 @@ class User(UserMixin, db.Model):
 		if self.query.filter_by(email=new_email).first():
 			return False
 		self.email = new_email
+		self.avatar_hash = hashlib.md5(
+			self.email.encode('utf-8')).hexdigest()
 		db.session.add(self)
 		return True
 
@@ -130,6 +142,19 @@ class User(UserMixin, db.Model):
 	
 	def is_administrator(self):
 		return self.can(Permission.ADMINISTER)
+
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
+
+	def gravatar(self, size=100, default='identicon', rating='g'):
+		if request.is_secure:
+			url = 'https://cdn.v2ex.com/gravatar'
+		else:
+			url = 'https://cdn.v2ex.com/gravatar'
+		hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+		return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+			url=url, hash=hash, size=size, default=default, rating=rating)
 
 	def __repr__(self):
 		return '<User %r>' % self.username
