91f71af1b00c689dfe9f2ddfc0f03981557b5ab9
Turner <fanshaojie2014@gmail.com>
2016-07-18 18:32:01 +0800

    for deploy

--- config.py
+++ config.py
@@ -3,16 +3,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	SSL_DISABLE = False
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	SQLALCHEMY_RECORD_QUERIES = True
-	MAIL_SERVER = 'smtp.163.com'
-	MAIL_PORT = '25'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	QA_MAIL_SUBJECT_PREFIX = '[QA]'
-	QA_MAIL_SENDER = 'QA Admin <fanshaojie168@163.com>'
+	QA_MAIL_SENDER = 'QA Admin <fanshaojie2014@gmail.com>'
 	QA_ADMIN = os.environ.get('QA_ADMIN')
 	ZHIHU_POSTS_PER_PAGE = 10
 	ZHIHU_FOLLOWERS_PER_PAGE = 30
@@ -37,10 +38,50 @@ class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+	@classmethod
+	def init_app(cls, app):
+		Config.init_app(app)
+
+		import logging
+		from logging.handlers import SMTPHandler
+		credentials = None
+		secure = None
+		if getattr(cls, 'MAIL_USERNAME', None) is not None:
+			credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+			if getattr(cls, 'MAIL_USE_TLS', None):
+				secure()
+		mail_handler = SMTPHandler(
+			mailhost=(cls.MIAL_SERVER, cls.MAIL_PORT),
+			fromaddr=cls.QA_MAIL_SENDER,
+			toaddrs=[cls.QA_ADMIN],
+			subject=cls.QA_MAIL_SUBJECT_PREFIX + 'Application Error',
+			credentials=credentials,
+			secure=secure)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
+
+class HerokuConfig(ProductionConfig):
+	SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+	@classmethod
+	def init_app(cls, app):
+		ProductionConfig.init_app(app)
+
+		from werkzeug.contrib.fixers import ProxyFix
+		app.wsgi_app = ProxyFix(app.wsgi_app)
+
+		# log to stderr
+		import logging
+		from logging import StreamHandler
+		file_handler = StreamHandler()
+		file_handler.setLevel(logging.WARNING)
+		app.logger.addHandler(file_handler)
+
 config = {
 	'development': DevelopmentConfig,
 	'testing': TestingConfig,
 	'production': ProductionConfig,
+	'heroku': HerokuConfig,
 	'default': DevelopmentConfig
 }
 
